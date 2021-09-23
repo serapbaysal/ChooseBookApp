61da34c675a8eccf637f6def54dfeb958200948f
serapbaysal <53875909+serapbaysal@users.noreply.github.com>
2021-09-23 23:48:51 +0300

    created mvc project

---
+++ Controllers/BookController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ChooseBookApp.Controllers
+{
+    public class BookController:Controller
+    {
+        public IActionResult methodName()
+        {
+          //TODO: Implement Realistic Implementation
+          return View();
+        }
+    }
+}
