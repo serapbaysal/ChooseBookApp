b26d89e601931121169248697d4e24da5770c72e
serapbaysal <serap.baysal@hotmail.com>
2021-09-24 17:23:09 +0300

    created Project and added Book model

---
+++ Controllers/BookController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChooseBookApp.Controllers
+{
+    public class BookController:Controller
+    {
+        public IActionResult Index()
+        {
+          //TODO: Implement Realistic Implementation
+          return View();
+        }
+        public IActionResult Create()
+        {
+          //TODO: Implement Realistic Implementation
+          return View();
+        }
+        public IActionResult Details()
+        {
+          //TODO: Implement Realistic Implementation
+          return View();
+        }
+        public IActionResult List()
+        {
+          //TODO: Implement Realistic Implementation
+          return View();
+        }
+    }
+}
